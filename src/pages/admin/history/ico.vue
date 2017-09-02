c8e4d8f8c40e469efe93c6d589c363e38f0baf6e
AmagiDDmxh <1107537248@qq.com>
2017-09-02 14:34:26 +0800

    Update(ICO & package): ico section updated and change package.json config.

--- src/pages/admin/history/ico.vue
+++ src/pages/admin/history/ico.vue
@@ -2,18 +2,16 @@
   table.table.data-table.table-bordered.table-striped
     thead
       th 资产名称
-      th 买入数量
       th 买入单价ANS
       th 总计ANS
       th(style={ width: '260px' })
     tbody
       tr(v-for="item in orders")
         td {{ item.name }}
-        td ({{ item.baseAmount }} + {{ item.earlyBird }})
         td {{ item.price }}
         td {{ item.amount }}
         td.td-btn
-          el-button.btn.ljbutton(:loading="item.loading", v-if="item.canCancel", @click="cancel(item)")  撤销
+          el-button.btn.ljbutton(:loading="item.loading", v-if="!item.redeem", @click="redeem(item)")  取回
 </template>
 
 <script>
@@ -25,18 +23,19 @@
       getOrders() {
         return this.$store.dispatch('GET_HISTORY', { name: 'ico' })
                    .then(orders => this.orders = orders['data'].map(i => { i.loading = false; return i }))
-                   .catch(e => this.$message.error('获取挂单失败！请稍后再试！'))
+                   .catch(e => this.$message.error('获取订单失败！请稍后再试！'))
       },
 
-      async cancel(item) {
+      async redeem(item) {
         await this.stopFetch()
-        setTimeout(() => this.$set(item, 'loading', true), 2000)
+        this.$set(item, 'loading', true)
         try {
-          const res = await this.$store.dispatch('CANCEL', { id })
+          const res = await this.$store.dispatch('REDEEM', { id })
           if (res) {
             this.startFetch()
-            this.$message.success('撤单成功！')
+            this.$message.success('取回成功！')
           }
+          this.$set(item, 'loading', false)
         } catch(e) {
           this.startFetch()
           this.$message.error(e.body.non_field_errors[0])
@@ -63,8 +62,3 @@
 
   }
 </script>
-
-<style lang="css">
-
-
-</style>
