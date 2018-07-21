6f9e5f0f3c94f43bb95a63efc098ef6e9119878b
503945930 <503945930@qq.com>
2018-07-21 10:28:41 +0800

    add nav

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -9,67 +9,83 @@
 </template>
 
 <script>
-  import oContent from '../../components/common/oContent/index.vue'
+import oContent from "../../components/common/oContent/index.vue";
 
-  export default {
-    components: { oContent },
+export default {
+  components: { oContent },
 
-    data: () => ({
-      banners: [
-        {
-          src: require('~images/app-download.jpg'),
-          preview: require('~images/app-download.jpg'),
-          dec: 'sea app download',
-          route: {
-            path: '//otcgo.cn/download/'
-          }
-        },
-        // {
-        //   src: require('~images/banner_KAC-ANS.jpg'),
-        //   preview: require('~images/banner_KAC-ANS-r.jpg'),
-        //   dec: '蓝鲸淘正式上线开拍币\\小蚁股交易对',
-        //   route: {
-        //     path: '/markets',
-        //     query: { 'class': 'kacneo' }
-        //   }
-        // }
-      ],
-      loading: false
-    })
-  }
+  data: () => ({
+    banners: [
+      {
+        src: require("~images/ong-ont.png"),
+        preview: require("~images/ong-ont.png"),
+        dec: "ong-ont",
+        route: {
+          path: "//otcgo.cn/download/"
+        }
+      },
+      {
+        src: require("~images/app-download.jpg"),
+        preview: require("~images/app-download.jpg"),
+        dec: "sea app download",
+        route: {
+          path: "//otcgo.cn/download/"
+        }
+      }
+      // {
+      //   src: require('~images/banner_KAC-ANS.jpg'),
+      //   preview: require('~images/banner_KAC-ANS-r.jpg'),
+      //   dec: '蓝鲸淘正式上线开拍币\\小蚁股交易对',
+      //   route: {
+      //     path: '/markets',
+      //     query: { 'class': 'kacneo' }
+      //   }
+      // }
+    ],
+    loading: false
+  })
+};
 </script>
 
 <style lang="stylus">
-  .el-table__body
-    text-align: center
+.el-table__body {
+  text-align: center;
+}
 
-  .el-table__header .is-leaf
-    text-align: center
+.el-table__header .is-leaf {
+  text-align: center;
+}
 
-@media (max-width: 500px)
-  .el-carousel
-    display none
-  form.navbar-right
-    display none
+@media (max-width: 500px) {
+  .el-carousel {
+    display: none;
+  }
 
-  .el-carousel__item img
-    width: 100%
-    height: 400px
+  form.navbar-right {
+    display: none;
+  }
 
-  .el-carousel__container > button
-    display: none
+  .el-carousel__item img {
+    width: 100%;
+    height: 400px;
+  }
 
-  /*  .carousel-control
-      background: #8e8e8e;
-      height: 100px;
-      line-height: 100px;
-      font-size: 50px !important;
-      width: 51px !important;
-      margin-top: 140px;
-      text-shadow: 0 1px 2px rgba(0, 0, 0, 0);
-      background-image: none;
+  .el-carousel__container > button {
+    display: none;
+  }
 
-    .carousel-inner img
-      width: 100%;
-      height: 400px;*/
+  /* .carousel-control
+        background: #8e8e8e;
+        height: 100px;
+        line-height: 100px;
+        font-size: 50px !important;
+        width: 51px !important;
+        margin-top: 140px;
+        text-shadow: 0 1px 2px rgba(0, 0, 0, 0);
+        background-image: none;
+  
+      .carousel-inner img
+        width: 100%;
+        height: 400px; */
+}
 </style>
