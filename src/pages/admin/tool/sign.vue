64af555bfdd7575da2255ed6e787ab50c75ed6ae
503945930 <503945930@qq.com>
2018-09-11 21:58:51 +0800

    fix

--- src/pages/admin/tool/sign.vue
+++ src/pages/admin/tool/sign.vue
@@ -18,15 +18,27 @@ export default {
         /*  
             1 长度为偶数
             2 只能含有0-9 a-f 
+            3 不包含空格
         */
+        if (this.signStrBefore.length == 0) {
+          this.$message.error("非法字符!");
+          return;
+        }
+
         if (this.signStrBefore.length % 2 != 0) {
           console.log("length");  
           this.$message.error("非法字符!");
           return;
         }
 
-        const pattern = /[a-f0-9]/i;
+        const pattern = /^[a-f0-9]+$/i;
         if (!pattern.test(this.signStrBefore)) {
+          this.$message.error("非法字符!");
+          return;
+        }
+
+        const reg =/\s/;
+        if (reg.test(this.signStrBefore)) {
           this.$message.error("非法字符!");
           return;
         }
